8323d5d224df331693b362d144a04cde160c0cab
takatto <pamoon2902@gmail.com>
2021-05-17 23:13:08 +0900

    Add project files.

---
+++ fileEmptier/MainForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace fileEmptier
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(273, 20);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browser";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(372, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 22);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Empty em";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(372, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 22);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Change name";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(231, 42);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(135, 20);
+            this.txtTo.TabIndex = 4;
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(62, 42);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(135, 20);
+            this.txtFrom.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Change";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "to";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(547, 64);
+            this.label3.TabIndex = 8;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 161);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPath);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "File emptier and name replacer~";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ fileEmptier/MainForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fileEmptier
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            if (result != DialogResult.OK)
+                return;
+
+            string folder = fbd.SelectedPath;
+
+            var rootFiles = Directory.GetFiles(folder);
+            txtPath.Text = folder;
+
+        }
+
+        byte[] bytes = Encoding.ASCII.GetBytes("takaneko");
+        bool isFaily = false;
+        string[] filePaths = null;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(txtPath.Text))
+            {
+                MessageBox.Show("Location is not selected.", "Setence.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult rusure = MessageBox.Show("Are you sure you want to empty all the files in selected folder?", "Yes no question.", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (rusure == DialogResult.Yes)
+            {
+                try
+                {
+                    filePaths = Directory.GetFiles(txtPath.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("The location is invalid!", "Setence.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                foreach (string filePath in filePaths)
+                {
+                    try
+                    {
+                        File.WriteAllBytes(filePath, bytes);
+                    } 
+                    catch { isFaily = true; }
+                }
+            }
+            else { return; }
+            if (isFaily)
+            {
+                isFaily = false;
+                MessageBox.Show("One or some of the files can not be emptied.", "Setence.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            } else
+                MessageBox.Show("Successful~", "Setence.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            isFaily = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtPath.Text) || String.IsNullOrEmpty(txtTo.Text) || String.IsNullOrEmpty(txtFrom.Text))
+            {
+                MessageBox.Show("Field(s) missing.", "Setence.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string to = txtTo.Text;
+            string from = txtFrom.Text;
+
+            DialogResult rusure = MessageBox.Show("Are you sure you want to replace the name of all the files in selected folder?", "Yes no question.", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (rusure == DialogResult.Yes)
+            {
+                try
+                {
+                    filePaths = Directory.GetFiles(txtPath.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("The location is invalid!", "Setence.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                foreach (string filePath in filePaths)
+                {
+                    try
+                    {
+                        System.IO.File.Move(filePath, filePath.Replace(from, to));
+                    }
+                    catch { isFaily = true; }
+                }
+            }
+            else { return; }
+            if (isFaily)
+            {
+                isFaily = false;
+                MessageBox.Show("One or some of the files can not be renamed.", "Setence.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            else
+                MessageBox.Show("Successful~", "Setence.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            isFaily = false;
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
